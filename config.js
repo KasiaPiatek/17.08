464d2ff799cbd1ee307ecd9286602ec890230c61
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-03-20 17:32:34 +0100

    .

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+	'GOOGLE_CLIENT_ID': '297062709682-2b7t2a6l7bbaarcd7d2v1pid6bh5jjo5.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '6Dk-dmUp8amH95rebmCV8PKZ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'	
+}
